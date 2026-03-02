47eb7cdcf8818ed93d331b3d4f04683599422b4b
Anna Ståhlberg <as228gn@student.lnu.se>
2026-03-02 14:16:54 +0100

    Adding a firebaseproject

---
+++ saas-adapter/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8LlVeCBjradiJkrPfa6IawrurzSMM9CM",
+  authDomain: "historical-walk-a5d3a.firebaseapp.com",
+  projectId: "historical-walk-a5d3a",
+  storageBucket: "historical-walk-a5d3a.firebasestorage.app",
+  messagingSenderId: "726112215514",
+  appId: "1:726112215514:web:7c08eef120587c216720e3",
+  measurementId: "G-HT41K1J7LF"
+}
